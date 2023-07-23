7d22170fc23272abc06803ff5f8ec4f3e5bb3af4
SvetlanaMelekhova <s.melekhova@lumos.mobi>
2023-07-23 09:42:55 +0300

    first react-typescript

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,8 @@
+export function AboutPage() {
+    return (
+        <div>
+            If you send an object like the code above, it will return you an object with a new id. remember that nothing
+            in real will insert into the database. so if you want to access the new id you will get a 404 error.
+        </div>
+    )
+}
